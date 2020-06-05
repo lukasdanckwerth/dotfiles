21e01e27f7f4c63401cc4c9e78ccb57e3b6357ca
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-06-05 05:19:32 +0200

    bugfix in install.sh

--- .files/scripts/install.sh
+++ .files/scripts/install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -u
 
-alias h="env GIT_WORK_TREE=$HOME GIT_DIR=$HOME/.files.git"
+# url of git repository to clone
+REPO_URL=https://github.com/lukasdanckwerth/dotfiles.git
 
+# allow usage of aliases
+shopt -s expand_aliases
+
+# create an alias to use git with environment variables
+alias h='env GIT_WORK_TREE=$HOME GIT_DIR=$HOME/.files.git'
+
+# create a new empty repository
 h git init
 
-h git remote add origin https://github.com/lukasdanckwerth/dotfiles.git
+# set the repository URL
+h git remote add origin "${REPO_URL}"
 
+# receive latest updates
 h git fetch
 
+# checkout master (in order the repository is not on master branch)
 h git checkout master
+
+# exit gracefully
+exit 0
