48cf8acff69d0b266c57d282571a1dd7d9edf14e
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-06-11 14:29:27 +0200

    update intro banner

--- .files/.files.sh
+++ .files/.files.sh
@@ -18,21 +18,16 @@ source "${DOTFILES}/.profile"
 
 LAST_UPDATE_DATE=$(date -r "${DOTFILES_LAST_PULL_PATH}" +'%F %T')
 
-echo '''
-
-      ###### ## ##    ######  #####
-      ##        ##    ##  ## ##
-      ###### ## ##    ######  #### 
- ###  ##     ## ##    ##         ##
- ###  ##     ## ##### ###### #####
-'''
-
-echo "          OS: $OS_NAME"
-echo "       SHELL: $SHELL_NAME"
-echo "     VERSION: $(dotfiles version)"
-echo " LAST UPDATE: $LAST_UPDATE_DATE"
 echo ""
+echo -e "\e[38;5;160m      ###### ## ##    ######  #####"
+echo -e "\e[38;5;202m      ##        ##    ##  ## ##"
+echo -e "\e[38;5;11m      ###### ## ##    ######  ####"
+echo -e "\e[38;5;10m ###  ##     ## ##    ##         ##"
+echo -e "\e[38;5;27m ###  ##     ## ##### ###### #####"
+echo -e "\e[39m"
+echo "$SHELL_NAME on $OS_NAME, v$(dotfiles version), $LAST_UPDATE_DATE"
 echo ""
+
 
 
 source "${DOTFILES}/scripts/check-for-upgrade.sh"
