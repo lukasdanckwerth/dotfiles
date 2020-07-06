2c3fa8e4d8ecd700a3a0a9fabd60c4447e5325a5
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-07-07 01:21:53 +0200

    -m bugfix in check-for-upgrade.sh

--- .files/scripts/upgrade.sh
+++ .files/scripts/upgrade.sh
@@ -14,12 +14,11 @@ GIT_RESULT=$(git pull)
 # print result
 echo "${GIT_RESULT}"
 
-if [[ ${GIT_RESULT} == "Already up to date." ]] ; then
-    exit 0
-fi
-
 # store date of update
 echo "$(date +%s)" > "${DOTFILES_LAST_PULL_PATH}"
 
-# (re)source the .files.sh file
-source "${HOME}/.files/.files.sh"
+if [[ ${GIT_RESULT} == "Already up to date." ]] ; then
+   exit 0
+else
+   source "${HOME}/.files/.files.sh"
+fi
